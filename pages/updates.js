dc7b29a7ef097b76c2c8a4ae042950f758d267f1
rorymcc8898 <98715020+rorymcclenahan@users.noreply.github.com>
2022-12-28 21:55:38 -0500

    Improve readability of updates.js

--- pages/updates.js
+++ pages/updates.js
@@ -1,12 +1,49 @@
 export default function Updates() {
-    const matches = [
-        {"date": "9/24", "opponent": "ECSU", "location": "AWAY", "result": "L", "score": "25-14"},
-        {"date": "10/1", "opponent": "BABSON", "location": "AWAY", "result": "L", "score": "60-3"},
-        {"date": "10/8", "opponent": "TRINITY", "location": "HOME", "result": "L", "score": "27-17"},
-        {"date": "10/15", "opponent": "AMHERST", "location": "HOME", "result": "W", "score": "43-0"},
-        {"date": "10/22", "opponent": "WILLIAMS", "location": "AWAY", "result": "W", "score": "21-0"},
-        {"date": "10/29", "opponent": "UNION", "location": "HOME", "result": "W", "score": "53-17"},
-    ]
+  const matches = [
+    // list of matches for the season
+    {
+      date: "9/24",
+      opponent: "ECSU",
+      location: "AWAY",
+      result: "L",
+      score: "25-14",
+    },
+    {
+      date: "10/1",
+      opponent: "BABSON",
+      location: "AWAY",
+      result: "L",
+      score: "60-3",
+    },
+    {
+      date: "10/8",
+      opponent: "TRINITY",
+      location: "HOME",
+      result: "L",
+      score: "27-17",
+    },
+    {
+      date: "10/15",
+      opponent: "AMHERST",
+      location: "HOME",
+      result: "W",
+      score: "43-0",
+    },
+    {
+      date: "10/22",
+      opponent: "WILLIAMS",
+      location: "AWAY",
+      result: "W",
+      score: "21-0",
+    },
+    {
+      date: "10/29",
+      opponent: "UNION",
+      location: "HOME",
+      result: "W",
+      score: "53-17",
+    },
+  ];
 
   return (
     <div id="updates" className="font-DIN border-b-1">
@@ -20,11 +57,13 @@ export default function Updates() {
               <h1 className="text-5xl pb-2 pt-5">2022 FALL CALENDAR</h1>
               <ul>
                 {matches.map((match) => (
-                    <li>
-                        <p className="py-2">{match.date} - {match.opponent}, {match.location} {match.result}, {match.score}</p>
-                    </li>
+                  <li>
+                    <p className="py-2">
+                      {match.date} - {match.opponent}, {match.location}{" "}
+                      {match.result}, {match.score}
+                    </p>
+                  </li>
                 ))}
-                
               </ul>
             </div>
           </div>
